1a84a51cc5571d0f3b1749f69845a4d873d7a33f
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-20 21:04:59 +1100

    auto update

--- dist/subtle-crypto-window.js
+++ dist/subtle-crypto-window.js
@@ -1,18 +1,12 @@
 
    (function (node) {
-       
-       
-   
+
    var cryptoWindow =  function (storage,storageKey){
        if (typeof window==='object' && typeof process==='undefined') {
            if (storage!==false) {
                window.keyStorage = window.keyStorage || window[storage||"localStorage"];
            } else {
-               var tempKeyStorage={};
-               window.keyStorage = {
-                   getItem : function(k) {return tempKeyStorage[k];},
-                   setItem : function(k,v) { return tempKeyStorage[k];},
-               };
+               window.keyStorage =fakeStorage() ;
            }
            cryptoWindow = function () {return window;};
            cryptoWindow.keyname_public  = !!storageKey ? storageKey+"-public"  : "uploads-public";
@@ -27,15 +21,27 @@
          directory: storage||"key_storage"
        }),
        subtle = webcrypto.subtle,
-       keyStorage = webcrypto.keyStorage,
+       keyStorage = storage ? webcrypto.keyStorage : fakeStorage() ,
        node_window = { crypto : { subtle : subtle }, keyStorage : keyStorage};
        
        cryptoWindow = function () {return node_window;};
+       cryptoWindow.keyname_public  = !!storageKey ? storageKey+"-public"  : "uploads-public";
+       cryptoWindow.keyname_private = !!storageKey ? storageKey+"-private" : "uploads-private";
        
        return node_window;
    };
    
+
    
+   
+   function fakeStorage() {
+       var tempKeyStorage={};
+       return {
+           getItem : function(k) {return tempKeyStorage[k];},
+           setItem : function(k,v) { return tempKeyStorage[k];},
+       };
+   }
+
    cryptoWindow.hardCodedPublic=hardCodedPublic;
    function hardCodedPublic (cb) {
        var win=cryptoWindow(false),subtle=win.crypto.subtle,
@@ -89,11 +95,26 @@
    }
    
    cryptoWindow.generateKeys=generateKeys;
-   function generateKeys(cb){
+   function generateKeys(encdec,cb){
+       if (typeof encdec==='function') {
+           cb=encdec;
+           encdec=false;
+       } else {
+           encdec=!!encdec;
+       }
+       var suffix=encdec ? '-crypto':'';
+       
        var win=cryptoWindow(),subtle=win.crypto.subtle,keyStorage=win.keyStorage;
        
+
        // generating RSA key
-       subtle.generateKey({
+       subtle.generateKey(
+           encdec ? {
+                        name: "RSA-OAEP",
+                        modulusLength: 2048, //can be 1024, 2048, or 4096
+                        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+                        hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+           }: {
            name: "RSASSA-PKCS1-v1_5",
            modulusLength: 1024,
            publicExponent: new Uint8Array([1, 0, 1]),
@@ -102,15 +123,15 @@
            }
          },
            false,
-           ["sign", "verify"]
+           encdec ? ["encrypt", "decrypt"] : ["sign", "verify"]
          )
          .then(function(keyPairs){
            /** 
             * saving private RSA key to KeyStorage
             * creates file ./key_storage/prvRSA-1024.json
             */
-           keyStorage.setItem(cryptoWindow.keyname_private, keyPairs.privateKey);
-           keyStorage.setItem(cryptoWindow.keyname_public, keyPairs.publicKey);
+           keyStorage.setItem(cryptoWindow.keyname_private+suffix, keyPairs.privateKey);
+           keyStorage.setItem(cryptoWindow.keyname_public+suffix, keyPairs.publicKey);
            
            subtle.exportKey(
                "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
@@ -125,16 +146,66 @@
    }
    
    cryptoWindow.getPrivate=getPrivate;
-   function getPrivate (cb) {
+   function getPrivate (encdec,cb){
+       if (typeof encdec==='function') {
+           cb=encdec;
+           encdec=false;
+       } else {
+           encdec=!!encdec;
+       }
+       var suffix=encdec ? '-crypto':'';
        var win=cryptoWindow(),subtle=win.crypto.subtle,keyStorage=win.keyStorage;
        
-       cb(keyStorage.getItem(cryptoWindow.keyname_private));
+       cb(keyStorage.getItem(cryptoWindow.keyname_private+suffix));
    }
    
    cryptoWindow.getPublic=getPublic;
-   function getPublic (cb) {
+   function getPublic (encdec,cb){
+       if (typeof encdec==='function') {
+           cb=encdec;
+           encdec=false;
+       } else {
+           encdec=!!encdec;
+       }
+       var suffix=encdec ? '-crypto':'';
        var win=cryptoWindow(),subtle=win.crypto.subtle,keyStorage=win.keyStorage;
-       cb(keyStorage.getItem(cryptoWindow.keyname_public));
+       cb(keyStorage.getItem(cryptoWindow.keyname_public+suffix));
+   }
+   
+   cryptoWindow.importPublic=importPublic;
+   function importPublic (keydata,encdec,cb,nosave){
+       if (typeof encdec==='function') {
+          cb=encdec;
+          encdec=false;
+       } else {
+          encdec=!!encdec;
+       }
+       var suffix=encdec ? '-crypto':'';
+       var win=cryptoWindow(),subtle=win.crypto.subtle,keyStorage=win.keyStorage;
+       
+       subtle.importKey(
+           "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+           keydata,
+           encdec ? 
+           {   //these are the algorithm options
+               name: "RSA-OAEP",
+               hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+           }:
+           {   //these are the algorithm options
+               name: "RSASSA-PKCS1-v1_5",
+               hash: {name: "SHA-1"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+           },
+           false, //whether the key is extractable (i.e. can be used in exportKey)
+           encdec ?["encrypt"] : ["verify"] 
+       )
+       .then(function(theKey){
+           //returns a publicKey (or privateKey if you are importing a private key)
+           if (!nosave) keyStorage.setItem(cryptoWindow.keyname_public+suffix)
+           cb(undefined,theKey);
+       })
+       .catch(cb);
+       
+       
    }
    
    cryptoWindow.exportPublic=exportPublic;
@@ -188,12 +259,48 @@
        .catch(cb);
    }
    
-   if (node) {
-      module.exports = cryptoWindow;
-   } else {
-       window.cryptoWindow = cryptoWindow;
+   
+   cryptoWindow.encrypt=encrypt;
+   function encrypt (_data,cb) {
+       var win=cryptoWindow(),subtle=win.crypto.subtle,keyStorage=win.keyStorage,
+       data = typeof _data ==='string'? Buffer.from(_data,"utf-8") : _data;
+       subtle.encrypt(
+           {
+               name: "RSA-OAEP",
+               //label: Uint8Array([...]) //optional
+           },
+           keyStorage.getItem(cryptoWindow.keyname_public+'-crypto'), 
+           data //ArrayBuffer of data you want to encrypt
+       )
+       .then(function(encrypted){
+           //returns an ArrayBuffer containing the encrypted data
+           cb(undefined,new Uint8Array(encrypted),data,_data);
+       })
+       .catch(cb);
    }
    
+   cryptoWindow.decrypt=decrypt;
+   function decrypt (_data,cb) {
+       var win=cryptoWindow(),subtle=win.crypto.subtle,keyStorage=win.keyStorage,
+       data = typeof _data ==='string'? Buffer.from(_data,"utf-8") : _data;
+       subtle.decrypt(
+           {
+               name: "RSA-OAEP",
+               //label: Uint8Array([...]) //optional
+           },
+           keyStorage.getItem(cryptoWindow.keyname_private+'-crypto'), 
+           data //ArrayBuffer of data you want to encrypt
+       )
+       .then(function(decrypted){
+           //returns an ArrayBuffer containing the decrypted data
+           cb(undefined,new Uint8Array(decrypted),typeof _data ==='string' ? new TextDecoder("utf-8").decode(decrypted):undefined);
+       })
+       .catch(cb);
+   }
+   
+
+   window.cryptoWindow = cryptoWindow;
+
    //generateKeys(console.log.bind(console,"generateKeys:"));
    //getPrivate (console.log.bind(console,"getPrivate:"));
    //getPublic (console.log.bind(console,"getPublic:"));
